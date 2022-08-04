25d1b824435a221b9fdfe447f19848a61bab6421
Marcos Corona <91139292+MarcosCorona@users.noreply.github.com>
2022-08-04 14:07:00 +0200

    Refractor

--- src/main/java/com/example/docker/postgresdocker/employees/infraestructure/controller/ApplicationController.java
+++ src/main/java/com/example/docker/postgresdocker/employees/infraestructure/controller/ApplicationController.java
@@ -14,42 +14,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
-@RequestMapping("/postgressApp")
+@RequestMapping("/empleado")
 public class ApplicationController {
-
 @Resource
 EmployeeDao employeeService;
-
-@GetMapping(value = "/employeeList")
-public List<Employee> getEmployees() {
-return employeeService.findAll();
-
-}
-
-@PostMapping(value = "/createEmp")
-public void createEmployee(@RequestBody Employee emp) {
- employeeService.insertEmployee(emp);
-
-}
-@PutMapping(value = "/updateEmp")
-public void updateEmployee(@RequestBody Employee emp) {
- employeeService.updateEmployee(emp);
-
-}
-@PutMapping(value = "/executeUpdateEmp")
-public void executeUpdateEmployee(@RequestBody Employee emp) {
- employeeService.executeUpdateEmployee(emp);
-
-}
-
-@DeleteMapping(value = "/deleteEmpById")
-public void deleteEmployee(@RequestBody Employee emp) {
- employeeService.deleteEmployee(emp);
-
-}
-
+@GetMapping
+public List<Employee> getEmployees() {return employeeService.findAll();}
+@PostMapping
+public void createEmployee(@RequestBody Employee emp) {employeeService.insertEmployee(emp);}
+@PutMapping
+public void updateEmployee(@RequestBody Employee emp) {employeeService.updateEmployee(emp);}
+@DeleteMapping
+public void deleteEmployee(@RequestBody Employee emp) {employeeService.deleteEmployee(emp);}
 
 }
